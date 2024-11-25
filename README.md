f279db3a841f25789ca8cc7e59541c420217cf21
Basile Marchand <basile.marchand@mat.mines-paristech.fr>
2024-11-25 20:33:30 +0100

    Initial commit with snake skeleton

---
+++ README.md
@@ -0,0 +1,74 @@
+# Jeu du Snake en C++ 
+
+Vous allez compléter un jeu classique de Snake en C++, directement jouable dans le terminal. Ce code ne ressemble pas à ce qu'on ferait dans la vraie vie mais il a le bon goût d'être simple à prendre en main. 
+
+## Structure du Projet
+
+Le code est découpé en plusieurs parties que vous devrez compléter. Voici un aperçu des éléments principaux pour mieux comprendre comment le jeu fonctionne.
+
+1. Le Background
+
+Le terrain de jeu est représenté par une grille 2D de dimensions `nx x ny` (par défaut, 50x25).
+
+Le background est stocké dans un tableau 1D (`std::vector<int>`), où chaque cellule de la grille correspond à une position donnée.
+Signification des valeurs dans le tableau :
+0 : Case vide.
+1 : Bordure du terrain.
+2 : Nourriture (que le serpent doit manger).
+3 : un case du serpent (symboliser par `o`)
+
+L'index dans le tableau 1D peut être calculé à partir des coordonnées 2D (x, y) comme suit :
+
+```cpp
+index = y * nx + x.
+```
+2. Le Serpent
+Le serpent est représenté par un vecteur de paires de coordonnées (`std::vector<std::pair<int, int>>`), où chaque paire correspond à une case occupée par le serpent sur la grille.
+
+La tête du serpent est le premier élément du vecteur. Le reste du vecteur correspond aux segments du corps, dans l'ordre.
+
+Le serpent se déplace à l'aide des touches suivantes :
+
+- z : Haut.
+- q : Gauche.
+- s : Bas.
+- d : Droite.
+
+Ces touches sont captées dans la fonction `snake_movement` que vous devez compléter pour convertir une touche en un vecteur directionnel :
+
+## Ce que Vous Devez Faire
+
+1. Compléter les fonctions vides
+2. Compiler et Lancer le Jeu. Pour compiler le jeu, utilisez la commande suivante dans un terminal :
+
+```bash
+g++ -o snake snake.cpp
+```
+
+Pour lancer le jeu, exécutez simplement l'exécutable :
+
+```bash
+./snake
+```
+
+## Règles du Jeu
+
+**Objectif** : Déplacez le serpent pour manger la nourriture (symbolisée par @).  
+**Règle de croissance** : Chaque fois que le serpent mange une nourriture, il grandit d'une case.  
+**Fin de partie** : Si le serpent entre en collision avec une bordure ou avec lui-même. Vous pouvez modifier la logique dans la fonction verifyBorder si vous le souhaitez.
+
+## Bonus pour Aller Plus Loin
+Pour les plus ambitieux, voici quelques idées de fonctionnalités supplémentaires à ajouter :
+
+- Possibilité de passer à travers les murs : Si le serpent atteint une bordure, il réapparaît de l'autre côté.
+- Augmentation de la vitesse : Réduire progressivement la valeur de lap pour rendre le jeu plus rapide.
+
+## Aide et Conseils
+Prenez le temps de bien comprendre le rôle de chaque fonction avant de commencer.
+
+***Le code compile en l'état, mais le jeu n'est pas encore fonctionnel.***
+
+Pour sortir du programme, vous pouvez utiliser la combinaison de touches `Ctrl + C`.
+
+
+Testez chaque fonctionnalité progressivement, en imprimant des messages dans la console pour vérifier votre logique.
---
+++ snake.cpp
@@ -0,0 +1,225 @@
+#include <iostream>
+#include <vector>
+#include <utility>
+#include <array>
+#include <thread>
+#include "sys/ioctl.h"
+#include "termios.h"
+#include "stdio.h"
+#include "time.h"
+#include "stdlib.h"
+
+namespace internal
+{
+  int keyEvent();
+  void frameSleep(const int &ms);
+  const char *cmd_clear = "clear";
+  void backgroundClear();
+  void printFrame(const int &nx, const int &ny, const std::vector<int> &bg);
+  void createFood(std::vector<int> &bg, std::array<int, 2> &food, const int &nx, const int &ny);
+  bool eatFood(std::array<int, 2> &food, std::vector<std::pair<int, int>> &snake);
+}
+
+std::vector<int> backgroundSetup(const int &nx, const int &ny)
+{
+  std::vector<int> bg(nx * ny, 0);
+  int i;
+  for (i = 0; i < nx; i++)
+  {
+    bg[i] = 1;
+  }
+  for (i = 0; i < nx; i++)
+  {
+    bg[(ny - 1) * nx + i] = 1;
+  }
+  for (i = 0; i < ny; i++)
+  {
+    bg[i * nx] = 1;
+  }
+  for (i = 0; i < ny; i++)
+  {
+    bg[i * nx + nx - 1] = 1;
+  }
+
+  return bg;
+}
+
+void add_snake(const std::vector<std::pair<int, int>> &snake, std::vector<int> &bg, int nx, int ny)
+{
+  // 👉️ Your code here 👈️
+}
+
+void remove_snake(const std::vector<std::pair<int, int>> &snake, std::vector<int> &bg, int nx, int ny)
+{
+  // 👉️ Your code here 👈️
+}
+
+std::array<int, 2> snake_movement(char key)
+{
+  // 👉️ Your code here 👈️
+}
+
+bool verifyBorder(const std::vector<std::pair<int, int>> &snake, int nx, int ny)
+{
+  // 👉️ Your code here 👈️
+}
+
+std::vector<std::pair<int, int>> setupSnake(int snake_len)
+{
+  std::vector<std::pair<int, int>> snake(snake_len);
+  int i;
+  for (i = 0; i < snake_len; i++)
+  {
+    snake[i].first = 10 + i;
+    snake[i].second = 10;
+  }
+  return snake;
+}
+
+void update_snake_coordinates(std::vector<std::pair<int, int>> &snake, bool eat, std::array<int, 2> dxdy)
+{
+  // 👉️ Your code here 👈️
+}
+
+void startGame(const int &lap, const int &nx, const int &ny, std::vector<std::pair<int, int>> &snake, std::vector<int> &bg)
+{
+  char key;
+  std::array<int, 2> dxdy = {1, 0};
+  std::array<int, 2> food = {0, 0};
+  internal::createFood(bg, food, nx, ny);
+  while (true)
+  {
+    internal::frameSleep(lap);
+    if (internal::keyEvent())
+    {
+      std::cin >> key;
+      dxdy = snake_movement(key);
+    }
+    internal::backgroundClear();
+    add_snake(snake, bg, nx, ny);
+    internal::printFrame(nx, ny, bg);
+    remove_snake(snake, bg, nx, ny);
+    bool out = verifyBorder(snake, nx, ny);
+    if (out == false)
+    {
+      std::cerr << "" << std::endl;
+      exit(1);
+    }
+    bool eat = internal::eatFood(food, snake);
+    if (eat)
+    {
+      internal::createFood(bg, food, nx, ny);
+    }
+    update_snake_coordinates(snake, eat, dxdy);
+  }
+}
+
+int main()
+{
+  const int nx = 50;
+  const int ny = 25;
+  const int lap = 200;
+  int snake_len = 3;
+
+  std::vector<int> background = backgroundSetup(nx, ny);
+  internal::printFrame(nx, ny, background);
+
+  std::vector<std::pair<int, int>> snake = setupSnake(snake_len);
+
+  startGame(lap, nx, ny, snake, background);
+  return 0;
+}
+
+// 💀💀💀💀💀💀💀💀 DON'T TOUCH CODE AFTER THIS LINE 💀💀💀💀💀💀💀💀💀💀 ///
+
+namespace internal
+{
+
+  static const int STDIN = 0;
+  static bool initialized = false;
+
+  // Comment ca va
+
+  int keyEvent()
+  {
+    if (!initialized)
+    {
+      termios term;
+      tcgetattr(STDIN, &term);
+      term.c_lflag &= ~ICANON;
+      tcsetattr(STDIN, TCSANOW, &term);
+      setbuf(stdin, NULL);
+      initialized = true;
+    }
+    int bytesWaiting;
+    // int bytesWaiting;
+    ioctl(STDIN, FIONREAD, &bytesWaiting);
+    return bytesWaiting;
+  }
+
+  void frameSleep(const int &ms)
+  {
+    std::this_thread::sleep_for(std::chrono::milliseconds(ms));
+  }
+
+  /////////////
+
+  void backgroundClear()
+  {
+    int out = system(cmd_clear);
+    if (out != 0)
+    {
+      std::cerr << "clear command failed" << std::endl;
+      exit(1);
+    }
+  }
+
+  void printFrame(const int &nx, const int &ny, const std::vector<int> &bg)
+  {
+    for (int j = 0; j < ny; j++)
+    {
+      for (int i = 0; i < nx; i++)
+      {
+        if (bg[i + j * nx] == 1)
+        {
+          std::cout << "#";
+        }
+        else if (bg[i + j * nx] == 2)
+        {
+          std::cout << "@";
+        }
+        else if (bg[i + j * nx] == 3)
+        {
+          std::cout << "o";
+        }
+        else
+        {
+          std::cout << " ";
+        }
+      }
+      std::cout << std::endl;
+    }
+  }
+
+  void createFood(std::vector<int> &bg, std::array<int, 2> &food, const int &nx, const int &ny)
+  {
+    if (food[0] == 0)
+    {
+      food[0] = rand() % (nx - 2) + 2;
+      food[1] = rand() % (ny - 2) + 2;
+      bg[food[1] * nx + food[0]] = 2;
+    }
+  }
+
+  bool eatFood(std::array<int, 2> &food, std::vector<std::pair<int, int>> &snake)
+  {
+    if ((food[0] == std::get<0>(snake[0])) && (food[1] == std::get<1>(snake[0])))
+    {
+      food[0] = 0;
+      food[1] = 0;
+      return true;
+    }
+    return false;
+  }
+
+}
